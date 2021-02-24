14260bd0730eb0a0ff48bad083ba860fe647a037
Ipunk Pradipta <ipung.munandar@gmail.com>
2021-02-24 16:33:02 +0700

    add funtion pagination, searchFilter and filtering

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const exceptionColumn = 'action';
 const requestDataExample = {
     draw: 1,
     columns: [
@@ -61,8 +62,66 @@ const requestDataExample = {
     start: 0,
     length: 10,
     search:{
-        value: '',
+        value: 'ss',
         regex: false,
     },
-    table: m_users
-} 
+    table: 'm_users',
+    params:[{
+        key:'deleted_by',
+        operator: "!=",
+        value:'IPUNG',
+    }]
+}
+
+//list of Column from request
+let aColumns = [];
+requestDataExample.columns.map((value) => {
+    if(value.name !== exceptionColumn) aColumns.push(value.name);
+});
+
+const buildPagination = (requestDataExample) => {
+    let sLimit = ''
+    if(requestDataExample.start !== null && requestDataExample.length !== -1){
+        sLimit += `LIMIT ${requestDataExample.start}, ${requestDataExample.length}`
+    }
+    return sLimit;
+}
+
+const buildSearchFiltering = (requestDataExample) => {
+    let sWhere = '';
+    if(typeof requestDataExample.search !== 'undefined' && requestDataExample.search.value != "")
+    {
+        sWhere = "WHERE (";
+        aColumns.map((value) => sWhere += `${value} LIKE '%${requestDataExample.search.value}%' OR `);
+        sWhere = sWhere.substring(0, sWhere.length -4);
+        sWhere += ')';
+    }
+    return sWhere;
+}
+
+// extra Filtering with "parameter" params
+const buildFiltering = (requestDataExample) => {
+    let sWhere = buildSearchFiltering(requestDataExample);
+    const parameters = requestDataExample.params; 
+    if(typeof parameters !== 'undefined'){
+        if(Array.isArray(parameters)){
+            let wString='';
+            parameters.map((param,index) => {
+                (param.value.toLowerCase() === 'null') ? wString += `${param.key} IS NULL AND ` : wString += `${param.key} ${param.operator} '${param.value}' AND `
+                if(index + 1 == parameters.length) wString = wString.substring(0,wString.length -5);
+            })
+            sWhere == '' ? sWhere = `WHERE ${wString}` : sWhere += ` AND ${wString}`  
+        }
+        return sWhere
+    }
+    return sWhere
+}
+
+const query = (requestDataExample) => {
+    const pagination = buildPagination(requestDataExample); 
+    const filtering = buildFiltering(requestDataExample);
+    return `${filtering} ${pagination}` 
+}
+
+console.log(query(requestDataExample))
+
